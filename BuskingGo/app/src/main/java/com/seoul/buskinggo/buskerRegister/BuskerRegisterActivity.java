e828414340cbf190617795d3f6ba9e2eeb260d4b
minkyoungAn <minkyoungan@gmail.com>
2018-09-12 22:53:27 +0900

    initial commit

---
+++ BuskingGo/app/src/main/java/com/seoul/buskinggo/buskerRegister/BuskerRegisterActivity.java
@@ -0,0 +1,15 @@
+package com.seoul.buskinggo.buskerRegister;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.seoul.buskinggo.R;
+
+public class BuskerRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busker_register);
+    }
+}
---
+++ BuskingGo/app/src/main/java/com/seoul/buskinggo/buskerconfigure/BuskerConfigureActivity.java
@@ -0,0 +1,13 @@
+package com.seoul.buskinggo.buskerconfigure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuskerConfigureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busker_configure);
+    }
+}
