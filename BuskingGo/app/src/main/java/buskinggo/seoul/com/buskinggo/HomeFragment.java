efb9d35c617aa3e23c15086f76a36baed6084523
minkyoungAn <minkyoungan@gmail.com>
2018-09-16 16:22:11 +0900

    fragment 오류 수정

--- BuskingGo/app/src/main/java/buskinggo/seoul/com/buskinggo/HomeFragment.java
+++ BuskingGo/app/src/main/java/buskinggo/seoul/com/buskinggo/HomeFragment.java
@@ -1,6 +1,7 @@
 package buskinggo.seoul.com.buskinggo;
 
 
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,7 +13,6 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
-
 
     public HomeFragment() {
         // Required empty public constructor
--- BuskingGo/app/src/main/java/buskinggo/seoul/com/buskinggo/MainActivity.java
+++ BuskingGo/app/src/main/java/buskinggo/seoul/com/buskinggo/MainActivity.java
@@ -3,6 +3,9 @@ package buskinggo.seoul.com.buskinggo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
@@ -16,9 +19,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        homeFragment = (HomeFragment) getSupportFragmentManager().findFragmentById(R.id.home_fragment);
+        homeFragment = new HomeFragment();
         myPageFragment = new MyPageFragment();
+
         bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().add(R.id.container, homeFragment).commit();
 
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -26,26 +31,33 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.action_home: // 홈
-
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        changeFragment(homeFragment);
                         break;
                     case R.id.busking_list: // 버스킹 목록
-
+                        //changeFragment(buskingListFragment); // 여기 들어가는 Fragment만 바꿔주면 됨!!
                         break;
                     case R.id.busking_regist: // 버스킹 등록
-
+                        //changeFragment(buskerRegistFragment);
                         break;
                     case R.id.seoul_street: // 거리예술존
-
+                        //changeFragment();
                         break;
                     case R.id.my_page: // 마이페이지
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, myPageFragment).commit();
+                        changeFragment(myPageFragment);
                         break;
                 }
                 return true;
             }
         });
 
+    }
+
+    public void changeFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        getSupportFragmentManager().popBackStack();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.addToBackStack(fragment.getTag());
+        fragmentTransaction.commit();
     }
 
 }
