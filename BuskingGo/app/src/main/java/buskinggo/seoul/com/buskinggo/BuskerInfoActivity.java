2644603f00554cfcffc4b16f1954146176db2749
minkyoungAn <minkyoungan@gmail.com>
2018-09-16 18:32:51 +0900

    버스커 상세정보 레이아웃 작업중

---
+++ BuskingGo/app/src/main/java/buskinggo/seoul/com/buskinggo/BuskerInfoActivity.java
@@ -0,0 +1,14 @@
+package buskinggo.seoul.com.buskinggo;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class BuskerInfoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busker_info);
+    }
+
+}
