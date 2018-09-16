f698f102ee74b115f8288ca6ea3734ecd3716fd9
minkyoungAn <minkyoungan@gmail.com>
2018-09-16 14:21:58 +0900

    네비게이션바/ fragment 추가 - (선택된 페이지의  item 선택 못하도록 막아야 함. 첫번째 fragment  hide 필요)

--- BuskingGo/app/src/main/java/buskinggo/seoul/com/buskinggo/MainActivity.java
+++ BuskingGo/app/src/main/java/buskinggo/seoul/com/buskinggo/MainActivity.java
@@ -1,13 +1,51 @@
 package buskinggo.seoul.com.buskinggo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    HomeFragment homeFragment;
+    MyPageFragment myPageFragment;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeFragment = (HomeFragment) getSupportFragmentManager().findFragmentById(R.id.home_fragment);
+        myPageFragment = new MyPageFragment();
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home: // 홈
+
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        break;
+                    case R.id.busking_list: // 버스킹 목록
+
+                        break;
+                    case R.id.busking_regist: // 버스킹 등록
+
+                        break;
+                    case R.id.seoul_street: // 거리예술존
+
+                        break;
+                    case R.id.my_page: // 마이페이지
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, myPageFragment).commit();
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
+
 }
