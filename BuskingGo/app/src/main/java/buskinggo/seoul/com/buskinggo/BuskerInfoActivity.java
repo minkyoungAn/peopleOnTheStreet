1b5b589d2c2ef8ccdb8e5cd60c666a0c8bce6243
minkyoungAn <minkyoungan@gmail.com>
2018-09-17 22:36:42 +0900

    버스커 상세정보 기능추가중

--- BuskingGo/app/src/main/java/buskinggo/seoul/com/buskinggo/BuskerInfoActivity.java
+++ BuskingGo/app/src/main/java/buskinggo/seoul/com/buskinggo/BuskerInfoActivity.java
@@ -2,6 +2,9 @@ package buskinggo.seoul.com.buskinggo;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class BuskerInfoActivity extends Activity {
 
@@ -9,6 +12,20 @@ public class BuskerInfoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_busker_info);
+
+        Toolbar toolbar = findViewById(R.id.busker_info_toolbar);
+        TextView tvName = findViewById(R.id.busker_info_name);
+        TextView tvJenre = findViewById(R.id.busker_info_jenre_item);
+        TextView tvPlace = findViewById(R.id.busker_info_place_item);
+        TextView ivFavorite = findViewById(R.id.busker_info_favorite);
+        TextView tvIntroduce = findViewById(R.id.busker_info_introduce_item);
+
+        // DB 저장된 값 로드
+        
+
+
+
+
     }
 
 }
